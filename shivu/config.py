9252a1e3e5fca6859d822d84b964cae01bbaed35
Itachiuchihaclan <158417056+Itachiychiha@users.noreply.github.com>
2024-02-13 11:29:29 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "6402009857", "6662904064"
     GROUP_ID = -1002078556251
     TOKEN = "5939669096:AAG9kYEZzUDP8TChuq23Ff1HQQ7Jp9Vr800"
-    mongo_url = "mongodb+srv://Obito:obito@obito.biudumi.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "rising_stars2"
     UPDATE_CHAT = "database2002"
