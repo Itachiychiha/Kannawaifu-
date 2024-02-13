b0b704a0ac9e55478ed29b1c7e48abb008d3864d
Itachiuchihaclan <158417056+Itachiychiha@users.noreply.github.com>
2024-02-13 10:31:27 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "6662904064"
+    sudo_users = "6402009857", "6662904064"
+    GROUP_ID = -1002078556251
+    TOKEN = "6826276106:AAHdXZH2StDYxgzBV2Bk6galkD6Ba96JinY"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "rising_stars2"
+    UPDATE_CHAT = "database2002"
+    BOT_USERNAME = "siesta217_bot"
+    CHARA_CHANNEL_ID = "-1002143589279"
+    api_id = 24658337
+    api_hash = "bf99242dbb7f3501f28d39f0a0383cbf"
 
     
 class Production(Config):
